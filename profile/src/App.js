1217203cb9174a2771443b6e5eb5c609ee821c7e
muhilhamfebr26 <muhilhamfebr2@gmail.com>
2024-04-03 14:02:55 +0700

    index commit

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Index from "./Page1/Index";
 import Portofolio from "./Page2/Portofolio";
 
 function App() {
   return (
     <div>
+      <Index />
       <Portofolio />
     </div>
   );
